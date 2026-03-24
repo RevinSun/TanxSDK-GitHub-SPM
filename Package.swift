82aeedac6060391694503abdae14db2f6af10a77
sunren <sr02310075@taobao.com>
2026-03-24 19:32:27 +0800

    提交3.7.26版本
    Signed-off-by: sunren <sr02310075@taobao.com>

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TanxSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // 对外只暴露 Wrapper（它依赖二进制 TanxSDK + 携带资源）
+        .library(name: "TanxSDK", targets: ["TanxSDK"]),
+        .library(name: "TNXASDK", targets: ["TNXASDK"]),
+        .library(name: "TanxMonitor", targets: ["TanxMonitor"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TanxSDKBinary",
+            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/TanxSDK/releases/download/3.7.26/TanxSDK.xcframework.zip",
+            checksum: "0f18002f09c409a5d977cae8e40298374be6ea41fff4044f17bc73f81fc06df4"
+        ),
+        .binaryTarget(
+            name: "TNXASDK",
+            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/TanxSDK/releases/download/3.7.26/TNXASDK.xcframework.zip",
+            checksum: "0bfe2bb2a8843394c0a3eca7652fe580e0c404a07f2b18f4d71e9b0999d04938"
+        ),
+        .binaryTarget(
+            name: "TanxMonitor",
+            url: "https://github.com/RevinSun/TanxSDK-GitHub-SPM/TanxSDK/releases/download/3.7.26/TanxMonitor.xcframework.zip",
+            checksum: "4ef853880d6db57fa6f44d218b7e2a35b3ca89002b6eecc805e406ae166bb6fd"
+        )
+    ]
+)
